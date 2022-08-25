3e2cb4e9ab5d1871de7be3543d16c1c3b61d8e92
Pedro Fernandez <pfernandez@payall.com.ve>
2022-08-25 11:04:05 -0400

    Clase 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-    PokemonModule,
-  ],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'public'),
+		}),
+
+		MongooseModule.forRoot('mongodb://localhost:6017/nest-pokemon'),
+
+		PokemonModule,
+	],
 })
 export class AppModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
-  }
+	constructor(
+		@InjectModel(Pokemon.name)
+		private readonly pokemonModel: Model<Pokemon>,
+	) {}
 
-  findAll() {
-    return `This action returns all pokemon`;
-  }
+	async create(createPokemonDto: CreatePokemonDto) {
+		createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
-  }
+		try {
+			const pokemon = await this.pokemonModel.create(createPokemonDto);
+			return pokemon;
+		} catch (error) {
+			if (error.code === 11000)
+				throw new BadRequestException(
+					`Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+				);
+			throw new InternalServerErrorException(
+				`Can't create Pokemon - Check server logs`,
+			);
+		}
+	}
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
-  }
+	findAll() {
+		return `This action returns all pokemon`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} pokemon`;
+	}
+
+	update(id: number, updatePokemonDto: UpdatePokemonDto) {
+		return `This action updates a #${id} pokemon`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} pokemon`;
+	}
 }
