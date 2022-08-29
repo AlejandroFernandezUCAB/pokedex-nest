798f840726a51050e71986f81aafa542c8185105
Pedro Fernandez <pfernandez@payall.com.ve>
2022-08-29 09:26:15 -0400

    Clase 13

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,20 +4,21 @@ import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interfaces';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-	private readonly axios: AxiosInstance = axios;
-
 	constructor(
 		@InjectModel(Pokemon.name)
 		private pokemonModel: Model<Pokemon>,
+
+		private readonly http: AxiosAdapter,
 	) {}
 
 	async executeSeed() {
 		await this.pokemonModel.deleteMany({});
 
-		const { data } = await this.axios.get<PokeResponse>(
+		const data = await this.http.get<PokeResponse>(
 			`https://pokeapi.co/api/v2/pokemon?limit=10000`,
 		);
 
